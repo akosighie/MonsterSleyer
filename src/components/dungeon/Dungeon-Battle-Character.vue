021940f488c1d53718a0dbda076af7ba10beb3f5
juliuscnicolas <juliuscnicolas@gmail.com>
2020-07-17 21:10:06 +0800

    added model style

--- src/components/dungeon/Dungeon-Battle-Character.vue
+++ src/components/dungeon/Dungeon-Battle-Character.vue
@@ -181,6 +181,262 @@ export default {
     }
 
 
+    /* wind shrine */
+
+    .avian {
+        position: relative;
+        height: 291px;
+        top: 195px;
+        left: 49px;
+    }
+
+    .gargoyle {
+        position: relative;
+        height: 364px;
+        top: 103px;
+        left: 175px;
+    }
+
+    .garuda {
+        position: relative;
+        height: 456px;
+    }
+
+    .hippogriff {
+        position: relative;
+        height: 449px;
+        top: 25px;
+        left: 115px;
+    }
+
+    .treasure-hunter {
+        position: relative;
+        height: 265px;
+        top: 211px;
+        left: 193px;
+    }
+
+    /* sunken ruins */
+
+    .slime {
+        position: relative;
+        height: 240px;
+        top: 263px;
+    }
+
+    .megadolon {
+        position: relative;
+        height: 364px;
+        top: 84px;
+        left: 100px;
+    }
+
+    .kraken {
+        position: relative;
+        height: 368px;
+        top: 143px;
+    }
+
+    .godzilla {
+        position: relative;
+        height: 481px;
+        left: 126px;
+    }
+
+    .leviathan {
+        position: relative;
+        height: 515px;
+        bottom: 32px;
+        left: 70px;
+    }
+
+    /* snow cave */
+
+    .jack-frost {
+        position: relative;
+        height: 194px;
+        top: 245px;
+        left: 263px;
+    }
+
+    .polar-bear {
+        position: relative;
+        height: 400px;
+        top: 116px;
+        left: 69px;
+    }
+
+    .yeti {
+        position: relative;
+        height: 390px;
+        top: 81px;
+    }
+
+    .werewolf {
+        position: relative;
+        height: 389px;
+        top: 101px;
+        left: 126px;
+    }
+
+    .winter-wyvern {
+        position: relative;
+        height: 400px;
+        top: 91px;
+        left: 23px;
+    }
+
+    .snow-queen {
+        position: relative;
+        height: 240px;
+        top: 201px;
+        left: 264px;
+    }
+
+    /* hell */
+    .satyr {
+        position: relative;
+        height: 240px;
+        top: 222px;
+        left: 247px;
+    }
+
+    .barbatos {
+        position: relative;
+        height: 360px;
+        top: 81px;
+        left: 136px;
+    }
+
+    .efreet {
+        top: 79px;
+        position: relative;
+        height: 369px;
+        left: 168px;
+    }
+
+
+    .cerberus {
+        position: relative;
+        height: 299px;
+        top: 176px;
+    }
+
+    .thanatos {
+        position: absolute;
+        top: 130px;
+        left: 91px;
+    }
+
+    .Hades {
+        position: relative;
+        height: 380px;
+        top: 100px;
+        left: 172px;
+    }
+
+    /* Dragon King's Lair */
+    
+    .black-dragon {
+        position: relative;
+        height: 443px;
+        right: 81px;    
+    }
+
+    .sky-dragon {
+        position: relative;
+        height: 533px;
+        right: 59px;
+        bottom: 50px;
+    }
+
+    .fire-dragon {
+        position: relative;
+        height: 491px;
+        bottom: 39px;
+        right: 49px;
+    }
+
+    .bahamut {
+        position: relative;
+        height: 485px;
+        bottom: 38px;
+        left: 41px;
+    }
+
+    /* labyrinth */
+    .baal {
+        position: relative;
+        height: 445px;
+        top: 62px;
+        left: 152px;
+    }
+
+    .Asmodeus {
+        position: relative;
+        height: 414px;
+        top: 55px;
+        left: 101px;
+    }
+
+    .astaroth {
+        position: relative;
+        height: 421px;
+        left: 137px;
+        top: 18px;
+    }
+
+    .amon {
+        position: relative;
+        height: 412px;
+        top: 56px;
+        left: 52px;
+    }
+
+    .beelzebub {
+        position: relative;
+        height: 486px;
+        bottom: 51px;
+        left: 14px;
+    }
+
+    
+    /* sanctuary */
+    
+    .surtur {
+        position: relative;
+        height: 396px;
+        top: 100px;
+        left: 157px;
+    }
+
+    .Artemis {
+        position: relative;
+        height: 256px;
+        top: 221px;
+        left: 159px;
+    }
+
+    .Poseidon {       
+        position: relative;
+        height: 326px;
+        top: 159px;
+        left: 19px;
+    }
+
+    .ares {
+        position: relative;
+        height: 323px;
+        top: 163px;
+        left: 178px;
+    }
+
+    .zues {
+        position: relative;
+        height: 280px;
+        top: 190px;
+        left: 238px;
+    }
 
 
     .archer {
@@ -217,7 +473,6 @@ export default {
         top: 205px;
         left: 98px;
     }
-    
 
     
 </style>
